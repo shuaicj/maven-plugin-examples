5d1a03b6ab711a4f4871aa721c54c7233e9bb336
shuaicj <shuaicj@gmail.com>
2017-08-02 15:20:44 +0800

    add maven-compiler-plugin example

---
+++ plugin-compiler/src/main/java/shuaicj/example/maven/plugin/compiler/Application.java
@@ -0,0 +1,14 @@
+package shuaicj.example.maven.plugin.compiler;
+
+/**
+ * Test maven-compiler-plugin.
+ *
+ * @author shuaicj 2017/08/02
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is in " + Application.class.getName());
+    }
+}
+
