9839c876d201d3e180e4eb4866cad22c17c99ebd
shuaicj <shuaicj@gmail.com>
2017-08-02 11:13:00 +0800

    add common lib

---
+++ common/src/main/java/shuaicj/example/maven/plugin/common/HelloMath.java
@@ -0,0 +1,17 @@
+package shuaicj.example.maven.plugin.common;
+
+/**
+ * Simple math methods.
+ *
+ * @author shuaicj 2017/08/02
+ */
+public class HelloMath {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
