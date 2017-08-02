1c4ae43fc6b3bdd4b7b2c0093f19b379f01fd0c9
shuaicj <shuaicj@gmail.com>
2017-08-02 15:21:02 +0800

    add maven-jar-plugin example

---
+++ plugin-jar/src/main/java/shuaicj/example/maven/plugin/jar/Application.java
@@ -0,0 +1,13 @@
+package shuaicj.example.maven.plugin.jar;
+
+/**
+ * Test maven-jar-plugin.
+ *
+ * @author shuaicj 2017/08/02
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is in " + Application.class.getName());
+    }
+}
