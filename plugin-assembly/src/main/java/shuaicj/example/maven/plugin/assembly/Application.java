e0a9ec1c8167632c26369e60f7e16c6a4e67258e
shuaicj <shuaicj@gmail.com>
2017-08-02 15:12:07 +0800

    add maven-assembly-plugin example

---
+++ plugin-assembly/src/main/java/shuaicj/example/maven/plugin/assembly/Application.java
@@ -0,0 +1,16 @@
+package shuaicj.example.maven.plugin.assembly;
+
+import shuaicj.example.maven.plugin.common.HelloMath;
+
+/**
+ * Test maven-assembly-plugin.
+ *
+ * @author shuaicj 2017/08/02
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        HelloMath math = new HelloMath();
+        System.out.println("This is in " + Application.class.getName() + " and 1 + 2 = " + math.add(1, 2));
+    }
+}
