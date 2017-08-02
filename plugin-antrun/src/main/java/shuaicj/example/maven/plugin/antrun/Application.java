a7a01fa8dd220b24383198d216840bc6e4b27e91
shuaicj <shuaicj@gmail.com>
2017-08-02 15:32:23 +0800

    add maven-antrun-plugin example

---
+++ plugin-antrun/src/main/java/shuaicj/example/maven/plugin/antrun/Application.java
@@ -0,0 +1,13 @@
+package shuaicj.example.maven.plugin.antrun;
+
+/**
+ * Test maven-antrun-plugin.
+ *
+ * @author shuaicj 2017/08/02
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("This is in " + Application.class.getName());
+    }
+}
